8170271aa5e676684a43164b29703da5636e763a
Damián Adams <contact@mianlabs.com>
2020-04-09 12:45:24 -0500

    Update index structure for consistency between sandbox and project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-/* CSS */
+/* START CORE CSS */
 
 import './scss/typography/_typography.scss';
-
-// import '@fortawesome/fontawesome-free/css/all.css';
 // Add fontawesome through CDN because import doesn't seem to work on codesandbox.io
-
+// import '@fortawesome/fontawesome-free/css/all.css';
 import 'daemonite-material/css/material.css'; // Use the daemonite-material CSS instead of Sass styles due to: https://github.com/Daemonite/material/issues/191
 
 import './scss/main.scss';
 
-/* JavaScript */
+/* END CORE CSS */
+
+/* START CORE JAVASCRIPT */
 
 import $ from 'jquery';
 import 'bootstrap';
@@ -17,3 +17,9 @@ import 'daemonite-material/js/material.js';
 
 window.$ = $;
 window.jQuery = $;
+
+/* END CORE JAVASCRIPT */
+
+/* START THIRD-PARTY JAVASCRIPT LIBRARIES */
+
+/* END THIRD-PARTY JAVASCRIPT LIBRARIES */
