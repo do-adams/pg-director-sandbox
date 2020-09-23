77222b95136dcfd9f1151cc495bd79e1393d60da
Damian <contact@mianlabs.com>
2020-09-23 14:18:12 -0500

    Update font-awesome and remove codesandbox cdn import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 /* START CORE CSS */
 
 import './scss/typography/_typography.scss';
-// Add fontawesome through CDN because import doesn't seem to work on codesandbox.io
-// import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import 'daemonite-material/css/material.css'; // Use the daemonite-material CSS instead of Sass styles due to: https://github.com/Daemonite/material/issues/191
 
 import './scss/main.scss';
